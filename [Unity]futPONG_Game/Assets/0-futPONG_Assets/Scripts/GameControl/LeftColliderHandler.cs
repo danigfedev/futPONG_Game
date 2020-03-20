a805672c7cf6c9c282e46b23f6471904591b0f82
Daniel Garcia Fernandez <danigfe.dev@gmail.com>
2020-03-20 16:29:32 +0100

    [Unity-setup] Old assets imported in project

---
+++ [Unity]futPONG_Game/Assets/0-futPONG_Assets/Scripts/GameControl/LeftColliderHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeftColliderHandler : MonoBehaviour {
+
+    #region Public_variables
+
+    public Text AwayScoreText;
+
+    #endregion
+
+    #region Private_variables
+
+    private int AwayScore;
+    #endregion
+
+
+    // Use this for initialization
+    void Start () {
+
+        //Initializing the score and printing it.
+        AwayScore = 0;
+        AwayScoreText.text = AwayScore.ToString();
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Away team scores. Increment the away score and print it
+        AwayScore++;
+        AwayScoreText.text = AwayScore.ToString();
+
+        //Add animation here, just to try with states or just play the animation
+    }
+}
---
+++ [Unity]futPONG_Game/Assets/0-futPONG_Assets/Scripts/GameControl/RightColliderHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RightColliderHandler : MonoBehaviour {
+
+    #region Public_variables
+
+    public Text LocalScoreText;
+
+    #endregion
+
+    #region Private_variables
+
+    private int LocalScore;
+    #endregion
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Initializing the score and printing it.
+        LocalScore = 0;
+        LocalScoreText.text = LocalScore.ToString();
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Away team scores. Increment the away score and print it
+        LocalScore++;
+        LocalScoreText.text = LocalScore.ToString();
+
+        //Add animation here, just to try with states or just play the animation
+    }
+}
