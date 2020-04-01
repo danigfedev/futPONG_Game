2def470343b7c067f3ac783622c9c0545008ece5
Daniel Garcia Fernandez <danigfe.dev@gmail.com>
2020-04-01 21:18:04 +0200

    Project rename

---
+++ futPONG_Unity/Assets/0-futPONG_Assets/Scripts/GameControl/Android_PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Android_PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 5;
+
+    #region ***** Private fields *****
+
+    private Rigidbody2D rb;
+    private bool leftButtonPressed = false;
+    private bool rightButtonPressed = false;
+    
+    #endregion
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 horDirection = GetinputAxisDirection();
+        rb.velocity = new Vector2(horDirection.x * playerSpeed, rb.velocity.y);
+
+    }
+
+    //Description: Handles Left button's Pointer UDownp event
+    public void LeftButton_PointerDown()
+    {
+        leftButtonPressed = true;
+    }
+
+    //Description: Handles Left button's Pointer Up event
+    public void LeftButton_PointerUp()
+    {
+        leftButtonPressed = false;
+    }
+
+    //Description: Handles Right button's Pointer Down Event
+    public void RightButton_PointerDown()
+    {
+        rightButtonPressed = true;
+    }
+
+    //Description: Handles Right button's Pointer Up event
+    public void RightButton_PointerUp()
+    {
+        rightButtonPressed = false;
+    }
+
+
+    //Description: Checks control buttons' status and calculates player's horizontal direction
+    //Outputs: horizontal direction.
+    private Vector2 GetinputAxisDirection()
+    {
+        if (leftButtonPressed) return new Vector2(-1, 0);
+        else if (rightButtonPressed) return new Vector2(1, 0);
+        return new Vector2(0, 0);
+   
+    }
+}
---
+++ futPONG_Unity/Assets/0-futPONG_Assets/Scripts/MainMenu/Android_MenuHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Android_MenuHandler : MonoBehaviour
+{
+    
+    public void PlayFriendlyMatch()
+    {
+        //TODO Load additively match scene
+        //TODO unload this scene
+
+        //For now I'll just do a simple load scene
+        SceneManager.LoadScene(1);
+    }
+
+}
